99fe7dfbfd36ffc209e077992ce1f0b78ff22e44
alkocurek7 <alkocurek7@gmail.com>
2026-02-21 13:39:49 -0500

    Initialize React application with NeuralShelfStudio

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NeuralShelfStudio from './NeuralShelfStudio.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NeuralShelfStudio />
+  </React.StrictMode>
+)
