63768ce06e48f1813bdc86d7a0161065cb0389b8
alkocurek7 <alkocurek7@gmail.com>
2026-02-21 13:41:29 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NeuralShelfStudio from './NeuralShelfStudio.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NeuralShelfStudio />
-  </React.StrictMode>
-)
